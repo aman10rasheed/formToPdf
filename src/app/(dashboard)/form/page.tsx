631113715e4bc3e00b39cad7629f0962d3f78526
aman10rasheed <amanrasheedpro@gmail.com>
2024-12-26 17:20:17 +0530

    pdfDownlad

--- src/app/(dashboard)/form/page.tsx
+++ src/app/(dashboard)/form/page.tsx
@@ -3,6 +3,7 @@
 import InputField from "@/components/InputField";
 import Rating from "@/components/Rating";
 import React, { useState } from "react";
+import ProgressCard from "./ProgressCard";
 
 type Props = {};
 
@@ -55,7 +56,7 @@ const Form = (props: Props) => {
   };
 
   return (
-    <div className="p-4">
+    <div className="p-4 flex gap-2">
       <form onSubmit={handleSubmit}>
         <div className="flex gap-4 justify-around">
           <InputField
@@ -256,13 +257,15 @@ const Form = (props: Props) => {
             placeholder="Enter the home work here..."
           />
         </div>
-        <button
+        {/* <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 mt-4"
           type="submit"
         >
           Submit
-        </button>
+        </button> */}
       </form>
+      <ProgressCard data={formData}/>
+      
     </div>
   );
 };
